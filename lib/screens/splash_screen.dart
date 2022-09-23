80b1624d9f14557499c586690608934215280807
banku27 <pankajparihar123321@gmail.com>
2022-09-23 23:46:11 +0530

    youtube api

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_api/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+        const Duration(seconds: 3),
+        () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    'https://icon-library.com/images/youtube-icon-black-and-white/youtube-icon-black-and-white-0.jpg'),
+                fit: BoxFit.contain),
+          ),
+        ),
+      ),
+    );
+  }
+}
